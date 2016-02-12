d68efb43aebc746b3c525e53ab70a5d5b5b4afa5
Benjamin Jacobs <contact@ttocsneb.com>
2016-02-11 19:34:16 -0700

    In the beginning, the code was without form and was void()

---
+++ desktop/src/com/ttocsneb/income/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ttocsneb.income.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ttocsneb.income.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
