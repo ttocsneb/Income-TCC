c3a2d7323a2c72e353473f3abf2623e52b43afa8
Benjamin Jacobs <contact@ttocsneb.com>
2016-03-22 12:43:27 -0600

    Create Inventory Project

---
+++ inventory/desktop/src/com/ttocsneb/inventory/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ttocsneb.inventory.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ttocsneb.inventory.Inventory;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Inventory(), config);
+	}
+}
