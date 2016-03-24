a343496725fe0fe1cb107f9267f57a39cea5c214
Benjamin Jacobs <contact@ttocsneb.com>
2016-03-24 12:46:55 -0600

    Set up program

--- inventory/core/src/com/ttocsneb/inventory/Inventory.java
+++ inventory/core/src/com/ttocsneb/inventory/Inventory.java
@@ -2,26 +2,68 @@ package com.ttocsneb.inventory;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.kotcrab.vis.ui.VisUI;
+import com.kotcrab.vis.ui.widget.VisLabel;
 
 public class Inventory extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+	/**
+	 * The stage contains/renders all objects held within it.
+	 */
+	Stage stage;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.badlogic.gdx.ApplicationAdapter#create()
+	 */
+	@Override public void create() {
+		//Create a stage with the dimensions of the window.
+		stage = new Stage(new ScreenViewport());
+		//Load the assets for the stage.
+		VisUI.load();
+		
+		//Create a test label for the window.
+		stage.addActor(new VisLabel("Hi!", Color.BLACK));
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.badlogic.gdx.ApplicationAdapter#render()
+	 */
+	@Override public void render() {
+		//Clear the screen with a white color
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		//update, and draw the stage.
+		stage.act();
+		stage.draw();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.badlogic.gdx.ApplicationAdapter#resize(int, int)
+	 */
+	@Override public void resize(int width, int height) {
+		//Resize the stage when the window resizes.
+		stage.getViewport().update(width, width);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.badlogic.gdx.ApplicationAdapter#dispose()
+	 */
+	@Override public void dispose() {
+		//unload the assets/stage when the program closes.
+		VisUI.dispose();
+		stage.dispose();
 	}
 }
--- inventory/desktop/src/com/ttocsneb/inventory/desktop/DesktopLauncher.java
+++ inventory/desktop/src/com/ttocsneb/inventory/desktop/DesktopLauncher.java
@@ -5,8 +5,17 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ttocsneb.inventory.Inventory;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	/**
+	 * @param arg
+	 */
+	public static void main(String[] arg) {
+		//Create a configuration for the window.
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		//Set the title of the window.
+		config.title = "Inventory - Level 3";
+		
+		//Create the window.
 		new LwjglApplication(new Inventory(), config);
 	}
 }
